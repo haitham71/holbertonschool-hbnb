d05819bd1ec5dbf4f0eae31b66284dfa50dcaa8c
i1SaSa <s9ny398335@gmail.com>
2026-02-17 15:19:22 +0300

    Refactor Place class to inherit from BaseModel and streamline initialization

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -1,12 +1,11 @@
 import uuid
 from datetime import datetime
+from .basemodel import BaseModel
 
 
-class Place():
+class Place(BaseModel):
     def __init__(self, title, description, price, latitude, longitude, owner):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        super().__init__()
         self.title = title
         self.description = description
         self.price = price
@@ -23,14 +22,3 @@ class Place():
     def add_amenity(self, amenity):
         """Add an amenity to the place."""
         self.amenities.append(amenity)
-
-    def save(self):
-        """Update the updated_at timestamp whenever the object is modified"""
-        self.updated_at = datetime.now()
-
-    def update(self, data: dict):
-        """Update the attributes of the object based on the provided dictionary"""
-        for key, value in data.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-        self.save()  # Update the updated_at timestamp
