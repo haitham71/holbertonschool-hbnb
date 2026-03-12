0597291650da1896d57425025e8513e43e2dc25d
i1SaSa <s9ny398335@gmail.com>
2026-03-13 01:39:44 +0300

    feat(repository): implement SQLAlchemy repository methods for CRUD operations

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
 from flask_jwt_extended import JWTManager
 import os
@@ -10,6 +11,14 @@ from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
 
 jwt = JWTManager()
+db = SQLAlchemy()
+
+
+def create_app(config_class=config.DevelopmentConfig):
+    #
+    # Existent code with app Flask instance
+    #
+    db.init_app(app)
 
 
 def create_app():
